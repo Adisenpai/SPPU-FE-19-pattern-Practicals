7d9c37992945685cc2caad615d1970cbd533dc27
Ketan Inamdar <91153995+inamdarketan@users.noreply.github.com>
2022-03-09 16:40:22 +0530

    Update PPS5.py

--- PPS5.py
+++ PPS5.py
@@ -8,11 +8,16 @@ sum = 0
 
 i = T
 
+# Using while loop to perform the desired operations on each digit.
 while i > 0:
+    # Following operation will give us the remainder which will be the last digit of the number.
     digit = i % 10
+    # This step will add the cube of the digit to the sum.
     sum += digit ** 3
+    # Finally this operation will store the quotient into i so that we can perform same operations on the next digits.
     i = i // 10
 
+# Checking if the number is Armstrong number or not and printing the result.
 if T == sum:
     print("The given number " + str(T) + " is an Armstrong number : ")
 else:
