ef24723870bebff93da4d8825f723eca135fbdb6
Ketan Inamdar <91153995+inamdarketan@users.noreply.github.com>
2022-03-09 22:04:16 +0530

    Add files via upload

---
+++ PPS6.py
@@ -0,0 +1,28 @@
+""" To simulate simple calculator that performs basic tasks such as addition, subtraction,
+multiplication and division with special operations like computing x^y
+and x!. """
+
+x = int(input("Enter the first number : "))
+y = int(input("Enter the second number : "))
+
+print("Operations: +, -, *, /, x^y, x!")
+
+chose = input("Chose the operation to be performed : ")
+
+if chose == "+":
+ print(x, "+", y, "=", x + y)
+elif chose == "-":
+ print(x, "-", y, "=", x - y)
+elif chose == "*":
+ print(x, "*", y, "=", x * y)
+elif chose == "/":
+ print(x, "/", y, "=", x / y)
+elif chose == "x^y":
+ print(x, "^", y, "=", x ** y)
+elif chose == "x!":
+ print(x, "!=", y, "=", x != y)
+else:
+ print("Invalid input")
+
+
+
