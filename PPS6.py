80a42f0dc62923ac3cecd4b7a25287788d880bc6
Ketan Inamdar <91153995+inamdarketan@users.noreply.github.com>
2022-03-09 22:06:04 +0530

    Update PPS6.py

--- PPS6.py
+++ PPS6.py
@@ -3,6 +3,7 @@ multiplication and division with special operations like computing x^y
 and x!. """
 
 x = int(input("Enter the first number : "))
+
 y = int(input("Enter the second number : "))
 
 print("Operations: +, -, *, /, x^y, x!")
@@ -11,16 +12,22 @@ chose = input("Chose the operation to be performed : ")
 
 if chose == "+":
  print(x, "+", y, "=", x + y)
+ 
 elif chose == "-":
  print(x, "-", y, "=", x - y)
+ 
 elif chose == "*":
  print(x, "*", y, "=", x * y)
+ 
 elif chose == "/":
  print(x, "/", y, "=", x / y)
+ 
 elif chose == "x^y":
  print(x, "^", y, "=", x ** y)
+ 
 elif chose == "x!":
  print(x, "!=", y, "=", x != y)
+ 
 else:
  print("Invalid input")
 
