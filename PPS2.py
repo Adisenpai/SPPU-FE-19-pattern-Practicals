23d32bd1427a8eeda73cdc912df13c6a657beb87
Ketan Inamdar <91153995+inamdarketan@users.noreply.github.com>
2022-03-09 16:14:48 +0530

    Update PPS2.py

--- PPS2.py
+++ PPS2.py
@@ -2,9 +2,9 @@
 momentum. Momentum is calculated as e=mc2 where m is the mass of the object and c is
 its velocity. """ 
 
-m = int(input("Enter mass of object in kgs :"))
+m = int(input("Enter mass of object in kgs : "))
 
-v = int(input("Enter velocity of an object m/s :"))
+v = int(input("Enter velocity of an object m/s : "))
 
 # Momentum, p = mv
 momentum = m * v
@@ -12,5 +12,5 @@ momentum = m * v
 # Mass-Energy Equivalence, E = mc² (c is velocity)
 MEeq = m * ( v ** 2)
 
-print("object momentum is :", momentum)
-print("Mass-Energy Equivalence :", MEeq)
+print("object momentum is : ", momentum)
+print("Mass-Energy Equivalence : ", MEeq)
